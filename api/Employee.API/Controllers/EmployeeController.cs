79ef26d7964c60eab9d38adc4aec2fe133939e2f
leoprietsch <leo.prietsch@gmail.com>
2022-11-04 09:22:20 -0300

    feat: create webapi and setup employee controller

---
+++ api/Employee.API/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+        public ClientController() { }
+
+        [HttpGet]
+        public ActionResult GetAll()
+          => Ok();
+
+        [HttpGet("{id}")]
+        public ActionResult GetById()
+          => Ok();
+
+        [HttpPost]
+        public ActionResult Create()
+          => Ok();
+
+        [HttpPut("{id}")]
+        public ActionResult Update()
+          => Ok();
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+          => Ok();
+    }
+}
