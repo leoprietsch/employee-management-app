7d5d0646df4aabc6f5cca0c25c9d811dee07486a
leoprietsch <leo.prietsch@gmail.com>
2022-11-06 17:17:14 -0300

    feat: create app entities

---
+++ app/src/entities/Employee.ts
@@ -0,0 +1,13 @@
+import { Gender } from "./Enums/Gender";
+import { Team } from "./Enums/Team";
+
+export default interface Employee {
+  id?: number;
+  name: string;
+  birthDate: Date;
+  gender: Gender;
+  email: string;
+  CPF: string;
+  startDate: string;
+  team?: Team;
+}
---
+++ app/src/entities/Enums/Team.ts
@@ -0,0 +1,5 @@
+export enum Team {
+  "Back-end" = 1,
+  "Front-end" = 2,
+  "Mobile" = 3,
+}
