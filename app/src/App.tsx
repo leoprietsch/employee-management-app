3271bd7708cd15db2a87943f164830cc97f41599
leoprietsch <leo.prietsch@gmail.com>
2022-11-07 13:40:52 -0300

    feat: add icons and delete confirmation popup

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,7 @@ import EmployeeDataGrid from "./components/EmployeeDataGrid";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <EmployeeDataGrid />
     </div>
   );
--- app/src/components/EmployeeDataGrid.tsx
+++ app/src/components/EmployeeDataGrid.tsx
@@ -1,17 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { Button } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 import Employee from "../entities/Employee";
 import { Team } from "../entities/Enums/Team";
 import { getAll, remove } from "../api/employeeClient";
 import moment from "moment";
+import {
+  AddOutlined,
+  DeleteForeverOutlined,
+  EditOutlined,
+} from "@mui/icons-material";
 
 function EmployeeDataGrid() {
   const [employees, setEmployees] = useState<Employee[]>([]);
+  const [open, setOpen] = React.useState(false);
 
   useEffect(() => {
     getAll().then((res) => setEmployees(res.data));
   }, []);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (id?: number) => {
+    if (id) {
+      remove(id)
+        .then(() => {
+          setEmployees((current) =>
+            current.filter((employee) => employee.id !== id)
+          );
+        })
+        .finally(() => setOpen(false));
+    } else setOpen(false);
+  };
 
   const columns: GridColDef[] = [
     {
@@ -63,26 +92,42 @@ function EmployeeDataGrid() {
             }}
           >
             <Button
-              style={{ background: "#7ce0d3" }}
+              style={{ background: "#dadada", color: "white" }}
               variant="contained"
               size="small"
             >
-              Edit
+              <EditOutlined />
             </Button>
             <Button
-              style={{ background: "#f4364c" }}
+              style={{ background: "#f4364c", color: "white" }}
               variant="contained"
               size="small"
-              onClick={() => {
-                remove(params.row.id).then(() => {
-                  setEmployees((current) =>
-                    current.filter((employee) => employee.id !== params.row.id)
-                  );
-                });
-              }}
+              onClick={handleClickOpen}
             >
-              Remove
+              <DeleteForeverOutlined />
             </Button>
+            <Dialog
+              open={open}
+              onClose={() => handleClose()}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">DELETE EMPLOYEE</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  This action is irreversible. Are you sure you want to delete
+                  this employee?
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={() => handleClose()} autoFocus>
+                  Cancel
+                </Button>
+                <Button onClick={() => handleClose(params.row.id)}>
+                  Confirm
+                </Button>
+              </DialogActions>
+            </Dialog>
           </div>
         );
       },
@@ -90,10 +135,16 @@ function EmployeeDataGrid() {
   ];
 
   return (
-    <div
-      className="grid-container"
-      style={{ flex: 1, overflow: "auto", margin: "100px" }}
-    >
+    <div className="grid-container">
+      <Button
+        className="btn-add"
+        style={{ background: "#7ce0d3", color: "white" }}
+        variant="contained"
+        size="large"
+      >
+        <AddOutlined />
+        ADD EMPLOYEE
+      </Button>
       <DataGrid
         autoHeight
         disableSelectionOnClick
