d79873c4da80ea93cb8ef16d6d4bf783bc9f6730
leoprietsch <leo.prietsch@gmail.com>
2022-11-08 08:03:00 -0300

    refactor: remove unused code

--- api/Employee.API/Helpers/Validation/CPFValidation.cs
+++ api/Employee.API/Helpers/Validation/CPFValidation.cs
@@ -5,8 +5,6 @@ namespace Employee.API.Helpers.Validation
 {
     public class CPF : ValidationAttribute
     {
-        private static int Size { get; set; } = 11;
-
         public override bool IsValid(object value)
         {
             string stringValue = value as string;
