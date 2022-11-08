86a1381498243f9cb2278e0d0d26eb2fe652c39a
leoprietsch <leo.prietsch@gmail.com>
2022-11-08 08:01:07 -0300

    test: add CPF Validation tests

---
+++ api/test/Employee.API.Tests/Helpers/Validation/CPFValidationTest.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using System;
+using Employee.API.Helpers.Validation;
+namespace Employee.API.Tests
+{
+    public class CPFValidationTest
+    {
+        [Fact]
+        public void CPFValidation_With_Incorrect_CPF_Return_IsValid_False()
+        {
+            var cpfValidation = new CPF();
+            bool result = cpfValidation.IsValid("");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void CPFValidation_With_Correct_CPF_Return_IsValid_True()
+        {
+            var cpfValidation = new CPF();
+            bool result = cpfValidation.IsValid("85953547072");
+            Assert.True(result);
+        }
+    }
+}
